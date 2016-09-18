8f90cecafd8ecfe519e2314f47e496a43e1c291a
Mark Williams <markzawill@gmail.com>
2016-09-18 12:40:12 -0500

    initial commit.  Created models for the API, had some questions about how the models should be set up.  Did not do any migrations of data yet.  Will find out best practice for utilizing AWS

---
+++ WeddingBack/src/WeddingBack/Models/Picture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeddingBack.Models
+{
+    public class Picture
+    {
+        public byte Picture { get; set; }
+        public string TwitterPost { get; set; }
+        public string InstaPost { get; set; }
+        public int UserId { get; set; }
+        public string Tags { get; set; }
+
+    }
+}
---
+++ WeddingBack/src/WeddingBack/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeddingBack.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string UserBio { get; set; }
+        public string TwitterHandle { get; set; }
+        public string InstagramHandle { get; set; }
+    }
+}
